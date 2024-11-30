a7f59ba401e7644113d0fcdd60c57dce600a2907
Federico Jose Diaz Guanoluiza <federicojose2000@gmail.com>
2024-11-30 11:54:38 +0100

    still not working

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -5,18 +5,12 @@ sleep 10
 if [ -f ./wp-config.php ]; then
 	echo "Wordpress is already installed"
 else
-	echo "Downloading WordPress..."
 	wp core download --allow-root
+	wp config create --dbname=${MYSQL_DATABASE} --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASSWORD} --dbhost=${MYSQL_HOST} --allow-root
 
-	echo "Creating Wordpress/Mariadb connection..."
-	wp config create --dbname=${WP_DB_NAME} --dbuser=${WP_DB_USER} --dbpass=${WP_DB_PASSWORD} --dbhost=${WP_DB_HOST} --allow-root
-
-	echo "Installing WordPress..."
 	wp core install --url=${DOMAIN} --title="WordPress Site" --admin_user=${WP_ROOT} --admin_password=${WP_ROOT_PASS} --admin_email=${WP_ROOT_EMAIL} --allow-root
 
-	echo "Creating a user..."
 	wp user create ${WP_USER} ${WP_USER_EMAIL} --user_pass=${WP_USER_PASSWORD} --allow-root
-	echo "Setting role for ${WP_USER}..."
 	wp user set-role ${WP_USER} editor
 	echo "Wordpress was correctly installed!"
 fi
