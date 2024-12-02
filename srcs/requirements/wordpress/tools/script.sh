7b013ecdfae33eaaf3cac87f57e008307f01c477
Federico Jose Diaz Guanoluiza <federicojose2000@gmail.com>
2024-12-02 17:27:42 +0100

    done donete clrinete

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -2,17 +2,16 @@
 
 
 if [ -f ./wp-config.php ]; then
-	echo "Wordpress is already installed"
+	echo "Wordpress ya está instalado"
 else
 	wp core download --allow-root
-	echo "${MYSQL_HOST}, ${MYSQL_PASSWORD}, ${MYSQL_USER}, ${MYSQL_DATABASE}"
 	wp config create --dbname=${MYSQL_DATABASE} --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASSWORD} --dbhost=${MYSQL_HOST} --allow-root
-
 	wp core install --url=${DOMAIN} --title=$WP_TITLE --admin_user=${WP_ROOT} --admin_password=${WP_ROOT_PASS} --admin_email=${WP_ROOT_EMAIL} --allow-root
-
 	wp user create ${WP_USER} ${WP_USER_EMAIL} --user_pass=${WP_USER_PASSWORD} --allow-root
 	wp user set-role ${WP_USER} editor
-	echo "Wordpress was correctly installed!"
+        wp theme install hexa --activate --allow-root
+
+	echo "Wordpress instalado correctamente"
 fi
 
 
